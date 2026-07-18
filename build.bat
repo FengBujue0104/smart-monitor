918613f52c6a955b136035be401562b89b075b85
Cid <1952444901@qq.com>
2026-07-18 09:17:45 +0800

    fix Windows batch packaging script

--- build.bat
+++ build.bat
@@ -1,34 +1,42 @@
 @echo off
-setlocal
+setlocal EnableExtensions
 cd /d "%~dp0"
 
-set GOOS=windows
-set GOARCH=amd64
-set CGO_ENABLED=0
+set "GOOS=windows"
+set "GOARCH=amd64"
+set "CGO_ENABLED=0"
 
-:: 解析 rsrc 路径（优先已安装的 %GOBIN%/%GOPATH%\bin\rsrc.exe）
-where rsrc >nul 2>nul && set RSRCCMD=rsrc
-if not defined RSRCCMD (
-    if exist "%~dp0tools\rsrc.exe" (set RSRCCMD="%~dp0tools\rsrc.exe") else (
-        echo [setup] 安装 rsrc ...
-        go install github.com/akavel/rsrc@latest
-        for /f "tokens=*" %%i in ('go env GOPATH') do set GOBIN=%%i\bin
-        set RSRCCMD="%GOBIN%\rsrc.exe"
-    )
-)
+set "RSRCCMD="
+where rsrc >nul 2>nul
+if not errorlevel 1 set "RSRCCMD=rsrc"
 
-echo [1/3] 生成清单资源 rsrc.syso ...
-%RSRCCMD% -manifest rsrc/app.manifest -o rsrc.syso
+if defined RSRCCMD goto :rsrc_ready
+
+for /f "delims=" %%i in ('go env GOPATH') do set "GOPATHDIR=%%i"
+if exist "%GOPATHDIR%\bin\rsrc.exe" set "RSRCCMD=%GOPATHDIR%\bin\rsrc.exe"
+if defined RSRCCMD goto :rsrc_ready
+
+if exist "%~dp0tools\rsrc.exe" set "RSRCCMD=%~dp0tools\rsrc.exe"
+if defined RSRCCMD goto :rsrc_ready
+
+echo [setup] Installing rsrc...
+go install github.com/akavel/rsrc@latest
+if errorlevel 1 goto :err
+if not exist "%GOPATHDIR%\bin\rsrc.exe" goto :err
+set "RSRCCMD=%GOPATHDIR%\bin\rsrc.exe"
+
+:rsrc_ready
+echo [1/3] Generating manifest resource...
+"%RSRCCMD%" -manifest rsrc/app.manifest -o rsrc.syso
 if errorlevel 1 goto :err
 
-echo [2/3] 构建单 exe ...
+echo [2/3] Building smonitor.exe...
 go build -ldflags="-s -w -H windowsgui" -o smonitor.exe
 if errorlevel 1 goto :err
 
-echo [3/3] 完成。产物: smonitor.exe
-for %%A in (smonitor.exe) do echo   大小: %%~zA 字节
-goto :eof
+for %%A in (smonitor.exe) do echo [3/3] Done: %%~zA bytes
+exit /b 0
 
 :err
-echo 构建失败。
+echo Build failed.
 exit /b 1
